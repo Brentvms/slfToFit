a908d6413d5c475d6d867684d4371b0908a76600
VincentKriek <kriek.v@gmail.com>
2024-05-25 12:41:21 +0200

    Created User entity

--- SlgToFit/SlfEntities/Computer.cs
+++ SlgToFit/SlfEntities/Computer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SlfToFit.SlfEntities
+﻿namespace SlfToFit.SlfEntities
 {
 	internal class Computer(string unit, string serial, string activityType, DateTime dateCode)
 	{
---
+++ SlgToFit/SlfEntities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SlfToFit.SlfEntities
+{
+	internal class User(int color, string gender, string type)
+	{
+		public readonly int Color = color;
+		public readonly string Gender = gender;
+		public readonly string Type = type;
+	}
+}

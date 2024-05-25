a80edb427c16e4b36054174a26cb75bdcb0e24fa
VincentKriek <kriek.v@gmail.com>
2024-05-25 12:36:25 +0200

    Created Computer entity

--- SlgToFit/Slf.cs
+++ SlgToFit/Slf.cs
@@ -4,11 +4,11 @@ namespace SlfToFit
 {
 	internal class Slf(DateTime fileDate, int revision, Computer computer, GeneralInformation generalInformation, Entry[] entries, Marker[] markers)
 	{
-		public readonly DateTime FileDate = fileDate;
-		public readonly int Revision = revision;
-		public readonly Computer Computer = computer;
-		public readonly GeneralInformation GeneralInformation = generalInformation;
-		public readonly Entry[] Entries = entries;
-		public readonly Marker[] Markers = markers;
+		public readonly DateTime FileDate = fileDate;									// Date on which the file was exported
+		public readonly int Revision = revision;										// ??
+		public readonly Computer Computer = computer;									// Computer entry
+		public readonly GeneralInformation GeneralInformation = generalInformation;		// General information entry
+		public readonly Entry[] Entries = entries;										// All record entries
+		public readonly Marker[] Markers = markers;										// Markers for (only) laps
 	}
 }
--- SlgToFit/SlfEntities/Computer.cs
+++ SlgToFit/SlfEntities/Computer.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace SlfToFit.SlfEntities
 {
-	internal class Computer
+	internal class Computer(string unit, string serial, string activityType, DateTime dateCode)
 	{
+		public readonly string Unit = UnitNameMap(unit);
+		public readonly string Serial = serial;
+		public readonly string ActivityType = activityType;
+		public readonly DateTime DateCode = dateCode;
+
+		private static string UnitNameMap(string unit)
+		{
+			switch (unit)
+			{
+				case "rox110": return "ROX GPS 11.0";
+				default: return "Sigma Cycling Computer";
+			}
+		}
 	}
 }

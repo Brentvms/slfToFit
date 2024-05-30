b3860568b8c0e9e42e534cc0923062ab57c58a49
VincentKriek <kriek.v@gmail.com>
2024-05-31 00:02:21 +0200

    Add Slf ToFitEncoder class

---
+++ SlgToFit/SflToFitEncoder.cs
@@ -0,0 +1,30 @@
+﻿using Dynastream.Fit;
+
+namespace SlfToFit
+{
+	public static class SlfToFitEncoder
+	{
+		public static void Encode(Slf slf, string outputPath)
+		{
+			FileStream? fitDest = null;
+			Encode? encoder = null;
+			Dynastream.Fit.DateTime timeCreated = new(System.DateTime.Now);
+
+			try
+			{
+				fitDest = new(outputPath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
+				encoder = new(ProtocolVersion.V20);
+				encoder.Open(fitDest);
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+			finally
+			{
+				encoder?.Close();
+				fitDest?.Close();
+			}
+		}
+	}
+}

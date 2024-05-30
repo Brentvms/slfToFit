9c7a212b7a75244215b10a575d107278115b650b
VincentKriek <kriek.v@gmail.com>
2024-05-31 00:11:59 +0200

    Added guid developer field

--- SlgToFit/SflToFitEncoder.cs
+++ SlgToFit/SflToFitEncoder.cs
@@ -15,6 +15,7 @@ namespace SlfToFit
 				fitDest = new(outputPath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
 				encoder = new(ProtocolVersion.V20);
 				encoder.Open(fitDest);
+				CreateDeveloperDataFields(encoder);
 			}
 			catch(Exception ex)
 			{
@@ -25,6 +26,28 @@ namespace SlfToFit
 				encoder?.Close();
 				fitDest?.Close();
 			}
+		}
+
+		private static void CreateDeveloperDataFields(Encode encoder)
+		{
+			// setup developerID
+			DeveloperDataIdMesg developerIdMesg = new();
+			byte[] appId = new Guid("b89b6d22-57bd-4d35-8063-99f06960c596").ToByteArray();
+			for (int i = 0; i < appId.Length; i++)
+			{
+				developerIdMesg.SetApplicationId(i, appId[i]);
+			}
+			developerIdMesg.SetDeveloperDataIndex(0);
+			developerIdMesg.SetApplicationVersion(010);
+			encoder.Write(developerIdMesg);
+
+			// create guid field
+			FieldDescriptionMesg guidFieldDescriptionMesg = new();
+			guidFieldDescriptionMesg.SetDeveloperDataIndex(0);
+			guidFieldDescriptionMesg.SetFieldDefinitionNumber(0);
+			guidFieldDescriptionMesg.SetFitBaseTypeId(FitBaseType.String);
+			guidFieldDescriptionMesg.SetFieldName(0, "guid");
+			encoder.Write(guidFieldDescriptionMesg);
 		}
 	}
 }

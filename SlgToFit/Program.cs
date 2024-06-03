8e3be0cbe62f96d2d4276f82d00bba8fbc0d9bf4
VincentKriek <kriek.v@gmail.com>
2024-06-04 00:03:35 +0200

    removed unneccesary code

--- SlgToFit/Program.cs
+++ SlgToFit/Program.cs
@@ -11,13 +11,8 @@ namespace SlfToFit
 		static void Main()
 		{
 			CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
-			//ApplicationConfiguration.Initialize();
-			//Application.Run(new Form1());
-
-			// test code for quick encode. NOT FOR PRODUCTION
-			Slf? slf = SlfParser.ParseSlf("C:\\Users\\kriek\\source\\repos\\VincentKriek\\slgToFit\\Activities\\2024_05_22__14_49.slf");
-			SlfToFitEncoder encoder = new SlfToFitEncoder();
-			encoder.Encode(slf, "C:\\Users\\kriek\\Downloads\\test.fit");
+			ApplicationConfiguration.Initialize();
+			Application.Run(new Form1());
 		}
 	}
 }
